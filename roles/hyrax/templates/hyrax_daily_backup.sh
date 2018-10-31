f20b0b9dfe92efd7ae243bf8127ab2c13dc5718d
Kevin Bowrin <kevin.bowrin@carleton.ca>
2018-10-31 10:13:07 -0400

    Fixed find command and output for backup scripts

--- roles/hyrax/templates/hyrax_daily_backup.sh
+++ roles/hyrax/templates/hyrax_daily_backup.sh
@@ -21,7 +21,7 @@
 (echo -n " size: " &&  du -hs "{{ hyrax_backups_directory }}/daily/" | cut -f 1) >> /var/log/hyrax/backup.log 2>&1
 checksum=$(md5sum {{ hyrax_backups_directory }}/daily/hyrax-backup.tar.gz | cut -d ' ' -f 1)
 datestamp=$(date --rfc-3339=date)
-(echo -n "$(date --rfc-3339=seconds) - add timestamp - " && mv "{{ hyrax_backups_directory }}/daily/hyrax-backup.tar.gz" "{{ hyrax_backups_directory }}/daily/$datestamp-hyrax-backup-md5-$checksum.tar.gz") >> /var/log/hyrax/backup.log 2>&1
+(echo -n "$(date --rfc-3339=seconds) - add timestamp - " && mv "{{ hyrax_backups_directory }}/daily/hyrax-backup.tar.gz" "{{ hyrax_backups_directory }}/daily/$datestamp-hyrax-backup-md5-$checksum.tar.gz" && echo "$datestamp-hyrax-backup-md5-$checksum.tar.gz") >> /var/log/hyrax/backup.log 2>&1
 
 # Delete older backups
-(echo -n "$(date --rfc-3339=seconds) - delete older daily backups - " && find "{{ hyrax_backups_directory }}/daily" -name='*.tar.gz' -type=f -mindepth=1 -maxdepth=1 -mtime=+7 -delete -print) >> /var/log/hyrax/backup.log 2>&1
+(echo -n "$(date --rfc-3339=seconds) - delete older daily backups - " && find "{{ hyrax_backups_directory }}/daily" -mindepth 1 -maxdepth 1 -mtime +7 -name '*.tar.gz' -type f -delete -print) >> /var/log/hyrax/backup.log 2>&1
