b63a55be54e594e111430c05b3d9c692347ee53e
Kevin Bowrin <kevin.bowrin@carleton.ca>
2018-11-02 10:07:48 -0400

    Fix fixity check script output

--- roles/hyrax/templates/hyrax_fixity_check.sh
+++ roles/hyrax/templates/hyrax_fixity_check.sh
@@ -4,4 +4,4 @@ cd /var/www/hyrax/hyrax-root/
 export SECRET_KEY_BASE={{ hyrax_secret_key_base }}
 export HYRAX_POSTGRESQL_PASSWORD={{ hyrax_postgresqldatabase_user_password }}
 export RAILS_ENV=production
-(echo -n "$(date --rfc-3339=seconds) " && /usr/local/bin/bundle exec rails runner "Hyrax::RepositoryFixityCheckService.fixity_check_everything") >> /var/log/hyrax/fixitycheck.log 2>&1
+(echo -n "$(date --rfc-3339=seconds) " && /usr/local/bin/bundle exec rails runner "Hyrax::RepositoryFixityCheckService.fixity_check_everything" && echo "success" || echo "failure") >> /var/log/hyrax/fixitycheck.log 2>&1
